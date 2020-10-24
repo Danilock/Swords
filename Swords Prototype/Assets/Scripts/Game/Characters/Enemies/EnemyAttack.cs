b8135874c7f61a3b4a7fd9599c0fbd613860e881
Danilock <yo1234re@gmail.com>
2020-10-24 19:44:09 -0400

    Player attack changed

--- Swords Prototype/Assets/Scripts/Game/Characters/Enemies/EnemyAttack.cs
+++ Swords Prototype/Assets/Scripts/Game/Characters/Enemies/EnemyAttack.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Vector2 attackAreaSize = new Vector2(2f, 2f);
+    [SerializeField] Transform attackAreaPoint;
+    EnemyController enemy;
+    PlayerController player;
+    private void Start()
     {
-        
+        player = FindObjectOfType<PlayerController>();
+        enemy = GetComponent<EnemyController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Draws the attack area and if detects the player then starts the attack animation.
+    /// </summary>
+    public void DrawAttackArea()
     {
-        
+
+    }
+
+    public void DoDamage()
+    {
+        bool touchedPlayer = Physics2D.OverlapBox(attackAreaPoint.position, attackAreaSize, 0f, LayerMask.GetMask("Player"));
+
+        if (touchedPlayer)
+        {
+            player.TakeDamage(enemy.enemyProfile.baseDamage);
+        }
     }
 }
